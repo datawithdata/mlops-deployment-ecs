38797512f0593e80cf27ddcb61860c757d064c1f
Viswateja Rayapaneni <viswatejaster@gmail.com>
2024-02-22 20:07:35 +0530

    getting instance id for launch template

--- functions/create-auto-scaling-group/create-asg.py
+++ functions/create-auto-scaling-group/create-asg.py
@@ -1,5 +1,8 @@
 import json
 import boto3 
+ec2_client = boto3.client('ec2', region_name='your-region')
+
+
 def get_instance_id(event):
      # TODO implement
     client = boto3.client('ec2')
@@ -14,6 +17,36 @@ def get_instance_id(event):
             )
     
     return response['body']['InstanceTypes'][0]['InstanceType']
+
+def create_launch_template(event):
+    # Define parameters
+    template_name = "my-launch-template"
+    image_id = "ami-0123456789abcdef"
+    instance_type = get_instance_id(event)
+
+    # Create Launch Template
+    response = ec2_client.create_launch_template(
+        LaunchTemplateName=template_name,
+        ImageId=image_id,
+        InstanceType=instance_type,
+        'BlockDeviceMappings': [
+        {
+            'DeviceName': '/dev/xvda',
+            'Ebs': {
+                'VolumeSize': 120
+            }
+        }
+             ]
+        )
+
+    # Print the response
+    print(f"Launch Template created: {response['LaunchTemplateName']}")
+
+
+def create_asg():
+
+    return 1
+
 def lambda_handler(event, context):
    
     return {
--- functions/task-definition/task-definition.py
+++ functions/task-definition/task-definition.py
@@ -65,31 +65,8 @@ def register_task(event):
 def lambda_handler(event, context):
     # TODO implement
     # Replace these with your actual values
-    table_name = os.environ['table_name']
 
-    # Get a reference to the table
-    table = dynamodb.Table(table_name)
-    try:
-        # Check if record exists
-        response = table.get_item(
-            Key={"registry-name": event['registry-name']})
-        if 'Item' not in str(response):
-            print("Creating new registry")
-            event['versions'][0]['version'] = 1
-            _ = table.put_item(Item=event)
-        else:
-            print("update existing registry")
-            response['Item']['versions'].append({'version': get_latest_version(
-                response), 's3_location': event['versions'][0]['s3_location'], 'Accurecy': event['versions'][0]['Accurecy']})
-            response = table.update_item(
-                Key={"registry-name": event['registry-name']},
-                UpdateExpression='SET versions = :versions',
-                ExpressionAttributeValues={
-                    ':versions':  response['Item']['versions']
-                }
-            )
-    except Exception as err:
-        print(str(err))
+    _ = register_task(event)
 
     return {
         'statusCode': 200,

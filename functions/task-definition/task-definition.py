6f945be690a1c0d0ee0f940b6ac77995c9f8b234
tejadata <viswatejaster@gmail.com>
2024-02-23 19:52:59 +0530

    creating cluster and task defintion

--- functions/task-definition/task-definition.py
+++ functions/task-definition/task-definition.py
@@ -10,7 +10,7 @@ import json
 import boto3
 import os
 
-client = boto3.resource('ecs')
+client = boto3.client('ecs')
 dynamodb = boto3.resource('dynamodb')
 
 
@@ -21,38 +21,42 @@ def get_registry_info(event):
         Key={"registry-name": event['registry-name']})
     version_lst = []
     for versions in response['Item']['versions']:
-        if versions['version'] == event['version']:
+        if versions['version'] == event['model-version']:
+            print(versions)
             version_lst = versions['ECR-info']
-            ecr_location = versions['ecr-name']
-    versions = [ver_lst['version'] for ver_lst in version_lst]
-    return for [versions.sort(),ecr_location]
+            ecr_location = versions['ECR-info'][0]['ecr-name']
+    versions = [int(ver_lst['version']) for ver_lst in version_lst]
+    versions.sort()
+    return [versions, ecr_location]
 
 
 def register_task(event):
     version = ""
     version_info = get_registry_info(event)
+    print("-----")
+    print(version_info)
     ecr_location = version_info[1]
     if event['ecr-version'] == "latest":
-        version=version_info[0][-1]
+        version = str(version_info[0][-1])
     else:
         version = event['ecr-version']
     # Define your task definition details
     task_definition = {
-        "family": event['registry-name'],  # Replace with your desired name
+        "family": event['registry-name'],
         "cpu": event['cpu'],
         "memory": event['memory'],
         "networkMode": "awsvpc",
-        "requiresCompatibilities": ["FARGATE", "EC2"],
+        "requiresCompatibilities": ["EC2"],
         "containerDefinitions": [
             {
                 # Replace with your container name
                 "name": event['registry-name'],
                 # Replace with your image URI
-                "image": ecr_location+":"+version[-1],
+                "image": ecr_location+":"+str(version[-1]),
                 "portMappings": [
                     {
                         "containerPort": 80,  # Replace with your container port
-                        "hostPort": 8080  # Replace with your desired host port
+                        "hostPort": 80  # Replace with your desired host port
                     }
                 ]
             }
@@ -60,6 +64,37 @@ def register_task(event):
     }
 
     response = client.register_task_definition(**task_definition)
+    return response
+
+
+def create_cluster(event):
+
+    response = client.create_cluster(
+        clusterName=event['registry-name'],
+        tags=[
+            {
+                'key': 'string',
+                'value': 'string'
+            },
+        ],
+        settings=[
+            {
+                'name': 'containerInsights',
+                'value': 'enabled'
+            },
+        ],
+        configuration={
+            'executeCommandConfiguration': {
+                'logging': 'OVERRIDE',
+                'logConfiguration': {
+                    'cloudWatchLogGroupName': event['registry-name']+"-logs",
+                    'cloudWatchEncryptionEnabled': False,
+                }
+            }
+        },
+
+    )
+    return 1
 
 
 def lambda_handler(event, context):
@@ -67,8 +102,16 @@ def lambda_handler(event, context):
     # Replace these with your actual values
 
     _ = register_task(event)
+    _ = create_cluster(event)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Successfully update')
     }
+
+
+if __name__ == "__main__":
+    file_path = "/Users/bhanuteja/ecs-automation/mlops-deployment-ecs/functions/config.json"
+    with open(file_path, "r") as file:
+        contents = json.loads(file.read())
+    lambda_handler(contents, 1)
